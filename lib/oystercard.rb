aef5a7cf0664adf5e6f0907f3930fe1437f93d10
Dan Holmes <dh@danielholmes.com>
2020-10-05 16:06:10 +0100

    add touching in/out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,11 @@
 class Oystercard
   LIMIT = 90
+  MINIMUM_FARE = 1
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -15,5 +17,18 @@ class Oystercard
   def deduct(amount)
     new_balance = @balance - amount
     @balance = new_balance
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise "You need at least #{MINIMUM_FARE} pound to touch in." if @balance < MINIMUM_FARE
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,28 +1,53 @@
 require "Oystercard"
 
 describe Oystercard do
-  describe "# balance" do
-    it "has a balance" do
+  describe "balance" do
+    it "starts at 0" do
       expect(subject.balance).to eq 0
     end
 
-    it "can add to its balance" do
+    it "can be can added to" do
       subject.top_up(10)
       expect(subject.balance).to eq 10
     end
 
-    it "cannot store more than 90 pounds" do
-      error = "You cannot store more than 90 pounds."
-      expect { subject.top_up(100) }.to raise_error error
+    it "has a limit" do
+      subject.top_up(Oystercard::LIMIT)
+      error = "You cannot store more than #{Oystercard::LIMIT} pounds."
+      expect { subject.top_up(1) }.to raise_error error
     end
 
-    it "can deduct money from its balance" do
+    it "can deducted from" do
       subject.top_up(10)
       subject.deduct(5)
       expect(subject.balance).to eq 5
     end
   end
 
-  describe "# touch in/out" do
+  describe "touch in/out" do
+    it "knows if it's in a journey" do
+      is_expected.not_to be_in_journey
+    end
+
+    context "when it has more than the minimum fare" do
+      before { subject.top_up(10) }
+      it "can touch in" do
+        subject.touch_in
+        is_expected.to be_in_journey
+      end
+
+      it "can touch out" do
+        subject.touch_in
+        subject.touch_out
+        is_expected.not_to be_in_journey
+      end
+    end
+
+    context "when it has less than the minimum fare" do
+      it "stops you touching in" do
+        error = "You need at least #{Oystercard::MINIMUM_FARE} pound to touch in."
+        expect { subject.touch_in }.to raise_error error
+      end
+    end
   end
 end
