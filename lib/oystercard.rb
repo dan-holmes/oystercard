1601ec65e9da7a946f5f48549c6fd135736d444c
Dan Holmes <dh@danielholmes.com>
2020-10-05 14:58:22 +0100

    can now add/deduct money from the balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,19 @@
 class Oystercard
+  LIMIT = 90
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    new_balance = @balance + amount
+    raise "You cannot store more than #{LIMIT} pounds." if new_balance > 90
+    @balance = new_balance
+  end
+
+  def deduct(amount)
+    new_balance = @balance - amount
+    @balance = new_balance
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,28 @@
 require "Oystercard"
 
 describe Oystercard do
-  it "has a balance" do
-    expect(subject.balance).to eq 0
+  describe "# balance" do
+    it "has a balance" do
+      expect(subject.balance).to eq 0
+    end
+
+    it "can add to its balance" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+    it "cannot store more than 90 pounds" do
+      error = "You cannot store more than 90 pounds."
+      expect { subject.top_up(100) }.to raise_error error
+    end
+
+    it "can deduct money from its balance" do
+      subject.top_up(10)
+      subject.deduct(5)
+      expect(subject.balance).to eq 5
+    end
+  end
+
+  describe "# touch in/out" do
   end
 end
